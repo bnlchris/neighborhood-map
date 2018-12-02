65e8d3cd24a2490fe14beea1c8cec3821ea00467
Christian Schultz <bnlchris@web.de>
2018-12-02 14:08:43 +0100

    loadGoogleMaps function in utils.js

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+//function to load Google Maps, credit to Ryan Waite: https://www.youtube.com/watch?v=5J6fs_BlVC0&feature=youtu.be
+export function loadGoogleMaps() {
+	return new Promise(function(resolve, reject) {
+		// run this when Google Maps is loaded
+		window.resolveGoogleMapsPromise = function() {
+			// resolve Google object
+			resolve(window.google);
+			// after that, delete callback
+			delete window.resolveGoogleMapsPromise;
+		}
+
+		// Load Google Maps API
+		const script = document.createElement('script');
+		const APIKey = 'AIzaSyCxZQ-z1otgbq-sDzm1ASKQ5TTdquwXu34';
+		script.src = 'https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise';
+		// Make sure, API is loaded asynchronously
+		script.async = true;
+		document.body.appendChild(script);
+	})
+}
