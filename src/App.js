1fe8d4be72be05346e55c5b28f6b5a93ae0840e8
Christian Schultz <bnlchris@web.de>
2018-12-02 14:09:18 +0100

    Implemented loadGoogleMaps function in App.js - loads data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { loadGoogleMaps } from './utils.js';
 
 class App extends Component {
+  
+  // load Google Maps
+  componentDidMount() {
+    let googleMapsPromise = loadGoogleMaps();
+    
+    Promise.all([
+      googleMapsPromise
+      ])
+      .then(values => {
+        console.log(values);
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Culinary Kiel</p>
+        
+        <header>
+          <p>Culinary Kiel</p>
+        </header>
+
+        <div id='map'>
+
+        </div>
+        
       </div>
     );
   }
