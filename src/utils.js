1246da3e57be9e01f1c246da8fda1ef08afaedbf
Christian Schultz <bnlchris@web.de>
2018-12-05 21:00:48 +0100

    Added loadFoursquarePlaces function to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-//function to load Google Maps, credit to Ryan Waite: https://www.youtube.com/watch?v=5J6fs_BlVC0&feature=youtu.be
+// function to load Google Maps, credit to Ryan Waite: https://www.youtube.com/watch?v=5J6fs_BlVC0&feature=youtu.be
 export function loadGoogleMaps() {
 	return new Promise(function(resolve, reject) {
 		// run this when Google Maps is loaded
@@ -16,4 +16,11 @@ export function loadGoogleMaps() {
 		script.async = true;
 		document.body.appendChild(script);
 	})
+}
+
+// function to load places from Foursquare
+export function loadFoursquarePlaces() {
+	// use API
+	const foursquareAPI = 'https://api.foursquare.com/v2/venues/search?client_id=E5CSHAOVG3VNBQFQBSZ0NS44BNEYP3ANKMUUK3LF4YF3ACO0&client_secret=RZG114RYPZYH0CQBBU1CHFPEDIG0N0J4S0PBJFW4VCYPBIRI&limit=10&near=Kiel&querypizza&v=20181206';
+	return fetch(foursquareAPI).then(resp => resp.json())
 }
