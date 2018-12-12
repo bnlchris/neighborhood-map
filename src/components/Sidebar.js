857a780c2e518a49d339a73d1fcdc1c5bc389fa5
Christian Schultz <bnlchris@web.de>
2018-12-12 21:10:24 +0100

    Put JSX of Sidebar into it's own component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+
+	constructor(props) {
+    	super(props);
+  	}
+
+	  render() {
+	  	return (
+	  		<div id='sidebar'>
+	          <input className='searchField' placeholder='Filter by name' value={this.props.query} onChange={(event) => {this.props.filterVenues(event.target.value)}}/>
+	          {
+	            this.props.filteredListOfVenues && this.props.filteredListOfVenues.length > 0 && this.props.filteredListOfVenues.map((venue, index) => (
+	              <div key={index} className='listOfPlaces' onClick={() => {this.props.showMarker(venue)}}>
+	                {venue.name}
+	              </div>
+	            ))
+	          }
+        	</div>
+	  	);
+	  }
+
+}
+
+export default Sidebar;
