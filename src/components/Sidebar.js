e453f0e1e8177560d1c554a9722220bf65c3932d
Christian Schultz <bnlchris@web.de>
2018-12-13 22:05:45 +0100

    Do not know what has changed here

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ class Sidebar extends Component {
 	  render() {
 	  	return (
 	  		<div id='sidebar'>
+
 	          <input className='searchField' placeholder='Filter by name' value={this.props.query} onChange={(event) => {this.props.filterVenues(event.target.value)}}/>
 	          {
 	            this.props.filteredListOfVenues && this.props.filteredListOfVenues.length > 0 && this.props.filteredListOfVenues.map((venue, index) => (
