0ad087b5a02be42e337a79f1da1e71228821a7ba
Christian Schultz <bnlchris@web.de>
2018-12-14 17:22:03 +0100

    Tried to implement the Close Sidebar method in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,11 @@ class Header extends Component {
 	render() {
 		return (
 			<header>
-          		<h1>Culinary Kiel</h1>
+          		<h1 className='headline'>Culinary Kiel</h1>
+          		<h1>
+	          		<i tabIndex='0' className="glyphicon glyphicon-remove"
+	          		title='Hide Sidebar' onClick={() => {this.props.hideSidebar()}}></i>
+          		</h1>
         	</header>
 		)
 	}
