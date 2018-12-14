2ae37ce491adf6ebc1c62e2cbb61406604f8c80d
Christian Schultz <bnlchris@web.de>
2018-12-14 19:59:19 +0100

    Changes on Service Worker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 
 // import file to start Service Worker
-import startServiceWorker from './startServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // Start Service Worker when page is online
-// startServiceWorker();
+serviceWorker.register();
