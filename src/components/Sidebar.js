9074d29f519ea6369a67bfef5e48dace392ba140
Christian Schultz <bnlchris@web.de>
2018-12-14 17:22:41 +0100

    Added Tab index to list items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,12 @@ class Sidebar extends Component {
 
 	  render() {
 	  	return (
-	  		<div id='sidebar'>
+	  		<div id='sidebar' aria-label='Sidebar'>
 
 	          <input className='searchField' placeholder='Filter by name' value={this.props.query} onChange={(event) => {this.props.filterVenues(event.target.value)}}/>
 	          {
 	            this.props.filteredListOfVenues && this.props.filteredListOfVenues.length > 0 && this.props.filteredListOfVenues.map((venue, index) => (
-	              <div key={index} className='listOfPlaces' onClick={() => {this.props.showMarker(venue)}}>
+	              <div key={index} tabIndex='0' className='listOfPlaces' onClick={() => {this.props.showMarker(venue)}}>
 	                {venue.name}
 	              </div>
 	            ))
