f0c148a6fdb5b78e763d90a941352d8b8fc44c59
Christian Schultz <bnlchris@web.de>
2018-12-15 20:09:21 +0100

    Simplified Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,6 @@ class Header extends Component {
 		return (
 			<header>
           		<h1 className='headline'>Culinary Kiel</h1>
-          		<h1>
-	          		<i tabIndex='0' className="glyphicon glyphicon-remove"
-	          		title='Hide Sidebar' onClick={() => {this.props.hideSidebar()}}></i>
-          		</h1>
         	</header>
 		)
 	}
